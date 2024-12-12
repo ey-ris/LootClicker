20967836f345abb2737c618a8aba20eae6478f1b
csumbdhyman <dhyman@csumb.edu>
2024-12-12 09:57:01 -0800

    Had* to rescope and downscale the project. Everything works. I'm tired. Added a basic Admin Landing page, it needs more beautifying and possibly a view to see all players and their stats.

---
+++ app/src/main/java/com/example/lootclicker/AdminActivity.java
@@ -0,0 +1,33 @@
+package com.example.lootclicker;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.lootclicker.database.AppRepository;
+import com.example.lootclicker.databinding.ActivityMainBinding;
+
+public class AdminActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+    private AppRepository repository;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        repository = AppRepository.getRepository(getApplication());
+    }
+
+    static Intent adminActivityIntentFactory(Context context) {
+        Intent intent = new Intent(context, MainActivity.class);
+        return intent;
+    }
+}
